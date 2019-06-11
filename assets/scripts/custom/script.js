faad3b844e4e779ab450ed8a5b781fe8e0766785
Nick Lemmon <nick.lemmon@gmail.com>
2019-06-10 21:40:20 -0400

    Remove smoothscroll script, add aria expanded toggling to mobile nav

--- assets/scripts/custom/script.js
+++ assets/scripts/custom/script.js
@@ -1,36 +1,11 @@
 $( document ).ready(function() {
-
   $('.MenuOpen').click(function() { // after the user click the menu hamburger icon...
-    $('.MainNavigation').toggleClass('open'); // toggle the navigation menu when clicking the menu icon
+    $('.MainNavigation')
+      .toggleClass('open')
+      .attr('aria-expanded', function (i, attr) {
+        return attr == 'true' ? 'false' : 'true'
+      });
   });
-
-    $('a[href*=#]:not([href=#])').click(function() { /* smooth scrolling function from https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 700);
-        return false;
-      }
-    }
-  });
-
-  var windowWidth = $(window).width();
-  if(windowWidth > 700){
-    var biggestHeight = "0";
-    // Loop through elements children to find & set the biggest height
-    $(".Section--media *").each(function(){
-     // If this elements height is bigger than the biggestHeight
-     if ($(this).height() > biggestHeight ) {
-       // Set the biggestHeight to this Height
-       biggestHeight = $(this).height();
-     }
-    });
-    // Set the container height
-    $(".Section--media").height(biggestHeight);
-  }
 
   $('.Accordion-header').click(function() {
     $accordionHeader = $(this);
@@ -46,10 +21,5 @@ $( document ).ready(function() {
       $accordionContent.toggleClass('is-expanded is-collapsed');
       $accordionHeader.toggleClass('is-expanded is-collapsed');
     }
-    
-    
   });
-
-    
-
 });
